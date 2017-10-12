54cfc06527dc174d1f765bf841c53c43f7be159e
Jairo <ji.bernal27@uniandes.edu.co>
2017-10-12 12:33:51 -0500

    Flight number added

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -1,24 +1,12 @@
 import React, { Component } from 'react';
  
-import Task from './Task.jsx';
 
 import Form from './Form.jsx';
  
 // App component - represents the whole app
 export default class App extends Component {
-  getTasks() {
-    return [
-      { _id: 1, text: 'This is task 1' },
-      { _id: 2, text: 'This is task 2' },
-      { _id: 3, text: 'This is task 3' },
-    ];
-  }
  
-  renderTasks() {
-    return this.getTasks().map((task) => (
-      <Task key={task._id} task={task} />
-    ));
-  }
+
  
   render() {
     return (
@@ -27,9 +15,7 @@ export default class App extends Component {
           <h1>Wheelchair reservation</h1>
         </header>
         <Form/>
-        <ul>
-          {this.renderTasks()}
-        </ul>
+       
       </div>
     );
   }
--- imports/ui/Form.jsx
+++ imports/ui/Form.jsx
@@ -9,7 +9,7 @@ export default class Form extends Component {
  
   constructor(props){
     super(props);
-    this.state={startDate: moment()};
+    this.state={startDate: moment(),flightNumber:""};
   }
 
   handleChange(date)
@@ -18,11 +18,66 @@ export default class Form extends Component {
       startDate: date
     });
   }
+  checkFlight(data)
+  {
+  	var input=data.target.value;
+  	this.setState({flightNumber:input});
+  	if (input.length==2)
+  	{
+  		if (/^[a-z0-9]+$/i.test(input))
+  		{
+  			this.setState({flightNumber:input.toUpperCase()});
+  		}
+  		
+  		else
+  		{
+  			this.setState({flightNumber:""});
+  		}
+
+  	}
+  	else if (input.length>=3 && input.length<=6)
+  	{
+  		var number =input.substring(2,input.length);
+  		if (/^[0-9]+$/i.test(number))
+  		{
+  			this.setState({flightNumber:input});
+  		}
+  		else
+  		{
+  			this.setState({flightNumber:input.substring(0,input.length-1)});
+  		}
+  	}
+  	else if (input.length>6)
+  	{
+  		this.setState({flightNumber:input.substring(0,input.length-1)});
+  		
+  	}
+  }
+
+  unblur()
+  {
+  	var fight=this.state.flightNumber.substring(0,2)
+  	var num=parseInt(this.state.flightNumber.substring(2,this.state.flightNumber.length));
+  	if (num)
+  	{
+  		this.setState({flightNumber:fight+num});
+  	}
+  	
+  }
+
+  sumbitForm(event)
+  {
+  	console.log(this.state);
+    event.preventDefault();
+  }
 
   render() {
     return (
     	<div className="form-input">
-    	<h2>Flying date</h2>
+    	<form>
+    	<label >
+    	 Flying date
+		<div id="datepicker-id">
 		<DatePicker
 		dateFormat="DDMMMYYYY"
 		minDate={moment()}
@@ -31,6 +86,14 @@ export default class Form extends Component {
         selected={this.state.startDate}
         onChange={this.handleChange.bind(this)} 
         placeholderText="Date selection"/>
+       </div>
+       </label>
+        <label>
+        Flight Number
+        <input type="text" name="name" onChange={this.checkFlight.bind(this)} value={this.state.flightNumber} onBlur={this.unblur.bind(this)}/>
+        </label>
+        <input type="submit" value="Search" onClick={this.sumbitForm.bind(this)} />
+        </form>
         </div>
     );
   }
