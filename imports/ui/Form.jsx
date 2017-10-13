798408002627684ad6a5642adb1aefe9899a26b0
Jairo <ji.bernal27@uniandes.edu.co>
2017-10-13 03:08:04 -0500

    Security

--- imports/ui/Form.jsx
+++ imports/ui/Form.jsx
@@ -66,42 +66,60 @@ export default class Form extends Component {
 
   sumbitForm(event)
   {
-  	console.log(this.state.startDate);
     event.preventDefault();
-    this.props.changeLoading();
-    Meteor.call('flightstats.status', {
-  	carrier: this.state.flightNumber.substring(0,2),
-  	flight: parseInt(this.state.flightNumber.substring(2,this.state.flightNumber.length)),
-  	year:this.state.startDate.get('year'),
-  	month:this.state.startDate.get('month')+1,
-  	day:this.state.startDate.get('date')}, (err, res) => {
-  	if (err) {
-    alert(err);
-    this.props.changeLoading();
-  	} else {
-    this.props.callbackFromparent(res);
-    this.props.changeLoading();
-  	}
-		});
+    carrier=this.state.flightNumber.substring(0,2);
+    flight=this.state.flightNumber.substring(2,this.state.flightNumber.length);
+
+    if(/^[a-z0-9]+$/i.test(carrier))
+    {
+      if (/^[0-9]+$/i.test(flight)&&flight.length<=4)
+      {
+            this.props.changeLoading();
+      Meteor.call('flightstats.status', {
+      carrier: this.state.flightNumber.substring(0,2),
+      flight: parseInt(flight),
+      year:this.state.startDate.get('year'),
+      month:this.state.startDate.get('month')+1,
+      day:this.state.startDate.get('date')}, (err, res) => {
+      if (err) {
+      alert(err);
+      this.props.changeLoading();
+      } else {
+      this.props.callbackFromparent(res);
+      this.props.changeLoading();
+      }
+      });
+      }
+      else
+      {
+        alert("wrong flight number");
+      }
+     
+    }
+    else
+      {alert("wrong carrier code");
+   }
+
+
   }
 
   render() {
     return (
     	<div className="form-input">
     	<form>
-    	<label >
+  <label >
     	 Flying date
 		<div id="datepicker-id">
-		<DatePicker
-		dateFormat="DDMMMYYYY"
-		minDate={moment()}
-  		maxDate={moment().add(12, "months")}
-  		monthsShown={2}
+		  <DatePicker
+		    dateFormat="DDMMMYYYY"
+		    minDate={moment()}
+  		  maxDate={moment().add(12, "months")}
+  		  monthsShown={2}
         selected={this.state.startDate}
         onChange={this.handleChange.bind(this)} 
         placeholderText="Date selection"/>
-       </div>
-       </label>
+    </div>
+  </label>
         <label>
         Flight Number
         <input type="text" name="name" onChange={this.checkFlight.bind(this)} value={this.state.flightNumber} onBlur={this.unblur.bind(this)}/>
--- server/methods.js
+++ server/methods.js
@@ -1,12 +1,29 @@
 
+import { check } from 'meteor/check';
+import { Match } from 'meteor/check';
 var url="https://api.flightstats.com/flex/flightstatus/rest/v2/json/flight/status/{carrier}/{number}/dep/{year}/{month}/{day}";
+
+const MonthCheck = Match.Where((x) => {
+  check(x, Match.Integer);
+  return x >= 1 ||x<=12;
+});
+
+const DayCheck = Match.Where((x) => {
+  check(x, Match.Integer);
+  return x >= 1 ||x<=31;
+});
+
 
 Meteor.methods({
 
   'flightstats.status'({carrier,flight,year,month,day}){
     this.unblock();
     try {
-
+        check(carrier, String);
+        check(flight,  Match.Integer);
+        check(year,  Match.Integer);
+        check(month,  MonthCheck);
+        check(day,  DayCheck);
       var urlAct=url.replace("{carrier}",carrier).replace("{number}",flight).replace("{year}",year).replace("{month}",month).replace("{day}",day);
       const result = HTTP.call('GET',urlAct, {
         params: { appId: process.env.APP_ID_FLI,appKey:process.env.APP_KEY_FLI }
@@ -22,8 +39,12 @@ Meteor.methods({
 
   'reserveChair.reserve'({date,flight,airportCode})
   {
-    this.unblock();
-
+    if(Meteor.userId())
+    {
+  this.unblock();
+    check(date,Date);
+    check(flight,String);
+    check(airportCode,String);
     var can=Reserves.find({owner:Meteor.userId(),date:date,flight:flight,airportCode:airportCode}).count();
     if (can==0)
     {
@@ -31,14 +52,21 @@ Meteor.methods({
       {
           Reserves.insert({owner:Meteor.userId(),date:date,flight:flight,airportCode:airportCode,createdAt:new Date()});
       }
-      }
+    }
+
+    }
+    
     
   },
 
     'reserveChair.delete'({date,flight,airportCode})
   {
-    this.unblock();
-
+    if (Meteor.userId())
+    {
+      this.unblock();
+    check(date,Date);
+    check(flight,String);
+    check(airportCode,String);
     var can=Reserves.find({owner:Meteor.userId(),date:date,flight:flight,airportCode:airportCode}).count();
     if (can!=0)
     {
@@ -47,5 +75,7 @@ Meteor.methods({
           Reserves.remove({owner:Meteor.userId(),date:date,flight:flight,airportCode:airportCode});
         }
     }
+    }
+    
   },
 });
